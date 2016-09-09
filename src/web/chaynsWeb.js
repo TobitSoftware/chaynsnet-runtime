8579e14d3bb4ef0aec0bc3045f7c731469f48481
Dennis Stockhofe <dennis.stockhofe@tobit.software>
2016-09-09 16:55:41 +0200

    overrides window.alert with chayns.dialog.alert

--- src/web/chaynsWeb.js
+++ src/web/chaynsWeb.js
@@ -2,15 +2,20 @@ import loadTapp from './customTapp';
 import {getUrlParameters} from '../config/customTapps';
 import Login from '../shared/login/login';
 import Textstrings from '../shared/utils/textstings';
-
+import Dialog from '../shared/dialog';
 
 document.addEventListener('DOMContentLoaded', () => {
     Textstrings.init().then(() => {
         let urlParameters = getUrlParameters();
-        let tappId = urlParameters && urlParameters.tappid ? urlParameters.tappid : '-7';
+        let tappId = urlParameters && urlParameters.tappid ? urlParameters.tappid : '-1';
         window.CustomTappCommunication.Init();
 
-        if (tappId === '-7') {
+        window.alert = (message, title) => Dialog.show('alert', {
+            title: title || null,
+            message
+        });
+
+        if (tappId === '-1') {
             setTimeout(() => Login.run().then((tobitAccessToken) => {
                 window.ChaynsInfo.User.TobitAccessToken = tobitAccessToken;
                 console.log('login-res', tobitAccessToken);
