0f77f368cfbb5f7627e5a546cb3666cbd62e388e
Kevin Mathmann <kevin.mathmann@tobit.software>
2016-09-09 16:35:38 +0200

    adds textstrings helper

---
+++ src/shared/utils/textstings.js
@@ -0,0 +1,50 @@
+export default class Textstrings {
+    static init() {
+        return new Promise((resolve, reject)=> {
+            chayns.utils.lang.init({
+                libs: [{
+                    project: 'DavidAdministration', //Project Name
+                    middle: 'LangRes'
+                }],
+                language: (navigator.language || navigator.userLanguage).substring(0, 2) || 'de',
+                preventOverride: false,
+                successCallback: ()=> {
+                    save();
+                    chayns.utils.lang.renderTextStrings(document.body);
+                    resolve();
+                },
+                errorCallback: reject
+            });
+        })
+    }
+
+    static get = {};
+}
+
+function save() {
+    Textstrings.get = {
+        login: {
+            loginfailed: chayns.utils.lang.get('txt_david_administration_login_loginfailed') || ''
+        },
+        intro: {
+            text: chayns.utils.lang.get('txt_david_administration_site_intro_text') || ''
+        },
+        information: {
+            duration: {
+                ends: chayns.utils.lang.get('txt_david_administration_site_information_duration_ends') || '',
+                trail: chayns.utils.lang.get('txt_david_administration_site_information_duration_trail') || '',
+                perpetual: chayns.utils.lang.get('txt_david_administration_site_information_duration_perpetual') || ''
+            },
+            payment: {
+                creditcard: chayns.utils.lang.get('txt_david_administration_site_information_payment_creditcard') || '',
+                bank: chayns.utils.lang.get('txt_david_administration_site_information_payment_bank') || ''
+            },
+            dialog: {
+                SiteCare: {
+                    confirm: chayns.utils.lang.get('txt_david_administration_site_information_dialog_sitecare_confirm') || '',
+                    cancelFailed: chayns.utils.lang.get('txt_david_administration_site_information_dialog_sitecare_cancelFailed') || ''
+                }
+            }
+        }
+    }
+}
