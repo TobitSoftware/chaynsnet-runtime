1fa7737114e4f9c543a0a79add5adcff5a6ae681
Kevin Mathmann <kevin.mathmann@tobit.software>
2016-09-09 16:34:45 +0200

    renames dialog(new).js to dialog.js

--- src/web/jsonCalls.js
+++ src/web/jsonCalls.js
@@ -1,4 +1,4 @@
-import DialogNew from '../shared/dialog(new)';
+import DialogNew from '../shared/dialog';
 import WaitCursor from '../shared/wait-cursor';
 import FloatingButton from '../shared/floating-button';
 import {argbHexToRgba} from '../shared/utils/convert';
