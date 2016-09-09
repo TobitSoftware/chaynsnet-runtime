448ac7ba7f09762cf68673d71d91fc9f22c5b46a
Kevin Mathmann <kevin.mathmann@tobit.software>
2016-09-09 16:37:27 +0200

    adds login and textstrings to chaynsWeb.js

--- src/web/chaynsWeb.js
+++ src/web/chaynsWeb.js
@@ -1,10 +1,22 @@
 import loadTapp from './customTapp';
 import {getUrlParameters} from '../config/customTapps';
+import Login from '../shared/login/login';
+import Textstrings from '../shared/utils/textstings';
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    let urlParameters = getUrlParameters();
-    let tappId = urlParameters && urlParameters.tappid ? urlParameters.tappid : '-7';
-    window.CustomTappCommunication.Init();
+    Textstrings.init().then(() => {
+        let urlParameters = getUrlParameters();
+        let tappId = urlParameters && urlParameters.tappid ? urlParameters.tappid : '-7';
+        window.CustomTappCommunication.Init();
 
-    loadTapp(tappId);
-}, false );
+        if (tappId === '-7') {
+            setTimeout(() => Login.run().then((tobitAccessToken) => {
+                window.ChaynsInfo.User.TobitAccessToken = tobitAccessToken;
+                console.log('login-res', tobitAccessToken);
+            }), 1500);
+        } else {
+            loadTapp(tappId);
+        }
+    });
+}, false);
