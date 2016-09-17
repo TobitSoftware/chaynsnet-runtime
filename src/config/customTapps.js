20542d53f690c6aeb488387c5bbf8d4ffba39e21
Kevin Mathmann <kevin.mathmann@tobit.software>
2016-09-17 16:47:46 +0200

    adds qaVersion of loginTapp

--- src/config/customTapps.js
+++ src/config/customTapps.js
@@ -5,7 +5,7 @@ let url = '?AppVersion=' + window.ChaynsInfo.Version + '&ColorScheme=' + window.
 export const loginTappId = '-1';
 
 export const config = {
-    '-1': 'http://w-mn.tobit.ag:7070?AppVersion=##version##&OS=##os##&colormode=##colormode##&color=##color##&tappid=-2',
+    '-1': 'http://tappqa.tobit.com/tapps/LoginTapp/?AppVersion=##version##&OS=##os##&colormode=##colormode##&color=##color##&tappid=-2',
     '-7': 'https://tapp01.tobit.com/Tapps/PayByApp/Home/Index?AppVersion=##version##&OS=##os##&colormode=##colormode##&color=##color##&tappid=-7',
     '251441': 'https://sub54.tobit.com/frontend/overview?AppVersion=##version##&OS=##os##&colormode=##colormode##&color=##color##&tappid=251441',
     '250357': 'https://tapp01.tobit.com/Tapps/ChaynsId/Tickets.html?AppVersion=##version##&OS=##os##&colormode=##colormode##&color=##color##&tappid=250357',
