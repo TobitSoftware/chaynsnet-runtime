58dae9bd71650dd5250a9e33e606ab57f6fe6357
Kevin Mathmann <kevin.mathmann@tobit.software>
2016-09-20 09:48:40 +0200

    fixes native-functions

--- src/shared/utils/native-functions.js
+++ src/shared/utils/native-functions.js
@@ -11,12 +11,15 @@ export function getAccessToken(accessToken) {
     }
 
     if (parentWindow.Chayns && 'GetAccessToken' in parentWindow.Chayns) {
-        functions.getAccessToken = document.parentWindow.external.Chayns.GetAccessToken;
+        functions.getAccessToken = () => document.parentWindow.external.Chayns.GetAccessToken();
+        console.log('david', !!functions.getAccessToken);
     } else if (defaultView.chayns && 'getAccessToken' in defaultView.chayns) {
-        functions.getAccessToken = document.defaultView.external.chayns.getAccessToken;
+        functions.getAccessToken = () => document.defaultView.external.chayns.getAccessToken();
+        console.log('clipinc', functions.getAccessToken);
     } else {
         functions.getAccessToken = () => getItem('*accessToken');
     }
+    console.log(functions.getAccessToken);
     return functions.getAccessToken();
 }
 
@@ -26,9 +29,9 @@ export function setAccessToken(accessToken) {
     }
 
     if (parentWindow.Chayns && 'SetAccessToken' in parentWindow.Chayns) {
-        functions.setAccessToken = document.parentWindow.external.Chayns.SetAccessToken;
+        functions.setAccessToken = (accessToken) => document.parentWindow.external.Chayns.SetAccessToken(accessToken);
     } else if (defaultView.chayns && 'setAccessToken' in defaultView.chayns) {
-        functions.setAccessToken = document.defaultView.external.chayns.setAccessToken;
+        functions.setAccessToken = (accessToken) => document.defaultView.external.chayns.setAccessToken(accessToken);
     } else {
         functions.setAccessToken = (accessToken) => setItem('*accessToken', accessToken);
     }
@@ -41,9 +44,9 @@ export function getKeyValue(key) {
     }
 
     if (parentWindow.Chayns && 'GetKeyValue' in parentWindow.Chayns) {
-        functions.getKeyValue = document.parentWindow.external.Chayns.GetKeyValue;
+        functions.getKeyValue = (key) => document.parentWindow.external.Chayns.GetKeyValue(key);
     } else if (defaultView.chayns && 'getKeyValue' in defaultView.chayns) {
-        functions.getKeyValue = document.defaultView.external.chayns.getKeyValue;
+        functions.getKeyValue = (key) => document.defaultView.external.chayns.getKeyValue(key);
     } else {
         functions.getKeyValue = getItem;
     }
@@ -56,9 +59,9 @@ export function setKeyValue(key, value) {
     }
 
     if (parentWindow.Chayns && 'PutKeyValue' in parentWindow.Chayns) {
-        functions.setKeyValue = document.parentWindow.external.Chayns.PutKeyValue;
+        functions.setKeyValue = (key, value) => document.parentWindow.external.Chayns.PutKeyValue(key, value);
     } else if (defaultView.chayns && 'putKeyValue' in defaultView.chayns) {
-        functions.setKeyValue = document.defaultView.external.chayns.putKeyValue;
+        functions.setKeyValue = (key, value) => document.defaultView.external.chayns.putKeyValue(key, value);
     } else {
         functions.setKeyValue = setItem;
     }
@@ -71,9 +74,9 @@ export function refreshChaynsIdIcons() {
     }
 
     if (parentWindow.Chayns && 'RefreshDisplay' in parentWindow.Chayns) {
-        functions.refreshChaynsIdIcons = document.parentWindow.external.Chayns.RefreshDisplay;
+        functions.refreshChaynsIdIcons = () => document.parentWindow.external.Chayns.RefreshDisplay();
     } else if (defaultView.chayns && 'RefreshDisplay' in defaultView.chayns) {
-        functions.refreshChaynsIdIcons = document.defaultView.external.chayns.refreshDisplay;
+        functions.refreshChaynsIdIcons = () => document.defaultView.external.chayns.refreshDisplay();
     } else {
         functions.refreshChaynsIdIcons = () => console.debug('refresh chaynsId icons');
     }
@@ -86,9 +89,9 @@ export function closeWindow() {
     }
 
     if (parentWindow.Window && 'Close' in parentWindow.Window) {
-        functions.closeWindow = document.parentWindow.external.Window.Close;
+        functions.closeWindow = () => document.parentWindow.external.Window.Close();
     } else if (defaultView.window && 'close' in defaultView.window) {
-        functions.closeWindow = document.defaultView.external.window.close;
+        functions.closeWindow = () => document.defaultView.external.window.close();
     } else {
         functions.closeWindow = () => console.debug('window closed.');
     }
@@ -101,9 +104,9 @@ export function resizeWindow(x, y) {
     }
 
     if (parentWindow.Window && 'ResizeTo' in parentWindow.Window) {
-        functions.resizeWindow = document.parentWindow.external.Window.ResizeTo;
+        functions.resizeWindow = () => document.parentWindow.external.Window.ResizeTo(x, y);
     } else if (defaultView.window && 'ResizeTo' in defaultView.window) {
-        functions.resizeWindow = document.defaultView.external.window.ResizeTo;
+        functions.resizeWindow = () => document.defaultView.external.window.ResizeTo(x, y);
     } else {
         functions.resizeWindow = (x, y) => console.debug(`resize window to x${x}, y${y}`);
     }
