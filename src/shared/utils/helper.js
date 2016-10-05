698a7259995de7d3ec254863a5c6d838a70fab0c
Kevin Mathmann <kevin.mathmann@tobit.software>
2016-10-05 13:39:40 +0200

    changes stylenames to npm package stylenames

--- src/shared/utils/helper.js
+++ src/shared/utils/helper.js
@@ -13,49 +13,6 @@ export function outerHeight(el) {
     return height;
 }
 
-/**
- * Convert Object to CSS style String.
- *
- *   Example:
- *      {
- *          height: "20px",
- *          width: {
- *              "20px": false,
- *              "30px": true,
- *              "40px": true
- *          }
- *      }
- *      returns: ' "height:20px;width:30px;" '
- *
- * @param styles
- * @returns {string}
- */
-export function styleNames(styles) {
-    let styleNames = '';
-
-    for (let key of Object.keys(styles)) {
-        if (typeof styles[key] === "string") {
-            styleNames += `${key}:${styles[key]};`;
-            continue;
-        }
-
-        if (typeof styles[key] !== "object" || styles[key].length === 0) {
-            continue;
-        }
-
-        let conditions = styles[key];
-
-        for (let value of Object.keys(conditions)) {
-            if (conditions[value]) {
-                styleNames += `${key}:${value};`;
-                break;
-            }
-        }
-    }
-
-    return `"${styleNames}"`;
-}
-
 export function setCookie(cName, value, exdays) {
     let exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
