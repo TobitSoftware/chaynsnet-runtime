3187498498f19171668f432292605b1a9240cd95
Kevin Mathmann <kevin.mathmann@tobit.software>
2016-10-17 15:34:23 +0200

    improves webpack configs
    adds qa config
    adds https dev-server
    adds NODE_ENV property

---
+++ webpack/qa.babel.js
@@ -0,0 +1,41 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const ROOT_PATH = path.resolve('./');
+
+export default {
+    entry: [
+        'babel-polyfill',
+        path.resolve(ROOT_PATH, 'src/index')
+    ],
+    resolve: {
+        extensions: ['', '.js', '.scss']
+    },
+    output: {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: 'chaynsweb.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: path.resolve(ROOT_PATH, 'src')
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass',
+                include: path.resolve(ROOT_PATH, 'src')
+            }
+        ]
+    },
+    devtool: 'hidden-source-map',
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('live')
+            }
+        })
+    ]
+};
