adf3a8ef77da1222aad61528b2c42d1d08e306f3
Kevin Mathmann <kevin.mathmann@tobit.software>
2017-03-10 10:12:31 +0100

    adds packages to dependencies instead of devDependencies

--- src/index.js
+++ src/index.js
@@ -11,9 +11,8 @@ import { isLIVE } from './constants/environments';
 
 import './style/index.scss';
 
-chayns.ready.catch((ex) => {
-    // ignore
-});
+// ignore chayns error
+chayns.ready.catch(() => false);
 
 logger.init({
     applicationUid: 'B150BF1E-A955-4073-B3DD-4F2CEC864C6A',
