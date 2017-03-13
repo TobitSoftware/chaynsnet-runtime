125dc78f2e5466b137962fdb26278001b0acab13
Kevin Mathmann <kevin.mathmann@tobit.software>
2017-03-13 14:10:34 +0100

    removes chayns js api

--- src/index.js
+++ src/index.js
@@ -14,9 +14,6 @@ import { isLIVE } from './constants/environments';
 
 import './style/index.scss';
 
-// ignore chayns error
-chayns.ready.catch(() => false);
-
 logger.init({
     applicationUid: 'B150BF1E-A955-4073-B3DD-4F2CEC864C6A',
     overrideOnError: true,
