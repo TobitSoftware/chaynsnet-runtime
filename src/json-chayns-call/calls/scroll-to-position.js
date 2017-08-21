a9f268828226c1e553bfcc825f5ff3812354d6b9
Kevin Mathmann <kevin.mathmann@gmail.com>
2017-08-21 12:58:42 +0200

    adds duration property to scroll-to-position chayns call

--- src/json-chayns-call/calls/scroll-to-position.js
+++ src/json-chayns-call/calls/scroll-to-position.js
@@ -2,6 +2,6 @@ import { scrollTo } from '../../utils/helper';
 
 export default function scrollToPosition(req, res) {
     if (req.value && 'position' in req.value) {
-        scrollTo(req.value.position);
+        scrollTo(req.value.position, req.value.duration);
     }
 }
