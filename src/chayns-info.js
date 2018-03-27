e2c20c1fde43d7badb4b64e0daaca75c5e4a5d41
Kevin Mathmann <kevin.mathmann@gmail.com>
2018-03-27 13:28:54 +0200

    Fix cache tapp does not handle if user logins
    The tapps are different for each user. I split the cache into a public and user cache. If a user is logged in the user cache is used and the object contains the userId.

--- src/chayns-info.js
+++ src/chayns-info.js
@@ -140,24 +140,24 @@ export async function updateUserData() {
     }
 }
 
-const tappsCacheKey = 'load-tapps_cache';
+const tappsCacheKeyPublic = 'load-tapps_cache_public';
+const tappsCacheKeyUser = 'load-tapps_cache_user';
 
 export async function loadTapps(locationId) {
     try {
-        const cache = getItem(tappsCacheKey);
+        const userId = chaynsInfo.User.ID === 0 ? null : chaynsInfo.User.ID;
+        const cacheKey = userId ? tappsCacheKeyUser : tappsCacheKeyPublic;
+        const cache = getItem(cacheKey);
 
-        const request = await Request.get(`https://chaynssvc.tobit.com/v0.5/${chaynsInfo.LocationID}/Tapp?forWeb=true${cache && cache.version === VERSION ? `&timestamp=${cache.timestamp}` : ''}`);
+        let cacheTimestamp = '';
 
-        if (request.status === 204 && !cache) {
-            consoleLoggerTapps.warn('Location has no tapps');
-            logger.warning({
-                message: 'Location has no tapps. (CustomNumber:Status)',
-                locationId,
-                customNumber: request.status,
-                fileName: 'chaynsInfo.js',
-                section: 'loadTapps',
-            });
-        } else if (request.status !== 200 && request.status !== 204) {
+        if (cache && cache.version === VERSION && (userId === null || (chaynsInfo.User && chaynsInfo.User.ID === cache.userId))) {
+            cacheTimestamp = `&timestamp=${cache.timestamp}`;
+        }
+
+        const request = await Request.get(`https://chaynssvc.tobit.com/v0.5/${chaynsInfo.LocationID}/Tapp?forWeb=true${cacheTimestamp}`);
+
+        if (request.status !== 200 && request.status !== 204) {
             consoleLoggerTapps.warn('Get locationTapps failed.', request.status);
             logger.error({
                 message: 'Get locationTapps failed. (CustomNumber:Status)',
@@ -168,11 +168,9 @@ export async function loadTapps(locationId) {
             });
         }
 
-        let tapps;
+        if (request.status === 200) {
+            consoleLoggerTapps.debug('loaded tapps from chaynssvc | key: ', cacheKey);
 
-        if (request.status === 204) {
-            tapps = cache.tapps;
-        } else {
             const jsonResponse = await request.json();
             const data = jsonResponse.data || [];
 
@@ -187,21 +185,26 @@ export async function loadTapps(locationId) {
                 return tapps;
             }, []);
 
-            tapps = getTappList(data);
+            const tapps = getTappList(data);
 
-            setItem(tappsCacheKey, {
+            chaynsInfo.Tapps = tapps;
+            setItem(cacheKey, {
                 version: VERSION,
                 timestamp: jsonResponse.timestamp,
+                userId,
                 tapps,
             });
+        } else {
+            consoleLoggerTapps.debug('apply tapps from cache | key: ', cacheKey);
+
+            chaynsInfo.Tapps = cache.tapps;
         }
 
-        tapps.push({
+        chaynsInfo.Tapps.push({
             id: LOGIN_TAPP_ID,
             url: chaynsInfo.loginTappUrl,
         });
 
-        chaynsInfo.Tapps = tapps;
         globalData.AppInfo.Tapps = chaynsInfo.Tapps;
     } catch (e) {
         logger.error({
