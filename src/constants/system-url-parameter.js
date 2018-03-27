2e2fd55f4e339a858223963907f2b442f3679158
Kevin Mathmann <kevin.mathmann@gmail.com>
2018-03-27 14:21:56 +0200

    Remove unused SYSTEM_URL_PARAMETERS entry

--- src/constants/system-url-parameter.js
+++ src/constants/system-url-parameter.js
@@ -1,3 +1,3 @@
-const SYSTEM_URL_PARAMETERS = ['locationid', 'tappid', 'navigation', 'accesstoken', 'debug', 'loglevel', 'login', 'forcelogin', 'backgroundcolor'];
+const SYSTEM_URL_PARAMETERS = ['locationid', 'tappid', 'navigation', 'accesstoken', 'debug', 'loglevel', 'forcelogin', 'backgroundcolor'];
 
 export default SYSTEM_URL_PARAMETERS;
