55c047b0cc592f0caea723e23ac821d84a5edb8f
Kevin Mathmann <kevin.mathmann@gmail.com>
2019-03-26 18:08:00 +0100

    Update babel and webpack
    Improve webpack configs

--- webpack/dev.babel.js
+++ webpack/dev.babel.js
@@ -1,12 +1,11 @@
-import { HotModuleReplacementPlugin, NoEmitOnErrorsPlugin, DefinePlugin } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import fs from 'fs';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-import BASE_CONFIG from './configs/base-config';
+import webpack from 'webpack';
+import merge from 'webpack-merge';
+import common from './common';
 
 const ROOT_PATH = path.resolve('./');
-
 
 const ssl = {};
 
@@ -14,35 +13,42 @@ try {
     ssl.cert = fs.readFileSync(path.join(__dirname, 'ssl', 'ssl.crt'));
     ssl.key = fs.readFileSync(path.join(__dirname, 'ssl', 'ssl.key'));
 } catch (e) {
+    // eslint-disable-next-line no-console
     console.log('\n---------------------------\nNo SSL Certificate found.\n---------------------------\n');
 }
 
-export default {
-    ...BASE_CONFIG,
-    devServer: {
-        cert: ssl.cert,
-        key: ssl.key,
-        host: '0.0.0.0',
-        port: 7070,
-        stats: {
-            colors: true
+export default merge(
+    common,
+    {
+        mode: 'development',
+        output: {
+            filename: '[name].bundle.js',
         },
-        compress: true,
-        disableHostCheck: true
-    },
-    devtool: 'inline-source-map',
-    plugins:
-        [
+        devServer: {
+            host: '0.0.0.0',
+            port: 7070,
+            historyApiFallback: true,
+            compress: true,
+            disableHostCheck: true,
+            cert: ssl.cert,
+            key: ssl.key,
+        },
+        devtool: 'inline-source-map',
+        plugins: [
+            new webpack.DefinePlugin({
+                __DEV__: true,
+                __STAGING__: false,
+                __PROD__: false,
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.LoaderOptionsPlugin({
+                debug: true
+            }),
+            new webpack.NamedModulesPlugin(),
+            new webpack.NoEmitOnErrorsPlugin(),
             new HtmlWebpackPlugin({
                 template: path.resolve(ROOT_PATH, 'index.ejs')
             }),
-            new DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify('development')
-                }
-            }),
-            new HotModuleReplacementPlugin(),
-            new NoEmitOnErrorsPlugin(),
         ]
-}
-;
+    }
+);
