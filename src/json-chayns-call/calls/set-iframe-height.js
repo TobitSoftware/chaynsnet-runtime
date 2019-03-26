8d92be0e368d8a2375a77fadf6855e18ecbff7d5
Kevin Mathmann <kevin.mathmann@gmail.com>
2019-03-26 12:06:31 +0100

    Change setIframeHeight full sets height to 100vh

--- src/json-chayns-call/calls/set-iframe-height.js
+++ src/json-chayns-call/calls/set-iframe-height.js
@@ -1,5 +1,4 @@
 import { chaynsInfo } from '../../chayns-info';
-import { getWindowMetrics } from '../../utils/helper';
 
 export default function setIframeHeigth(req, res) {
     const $iframe = req.srcIframe[0];
@@ -9,21 +8,22 @@ export default function setIframeHeigth(req, res) {
         res.event(2, 'Field height missing.');
         return;
     } else if (value.full || value.fullViewport || chaynsInfo.fullSizeMode) {
-        value.height = getWindowMetrics().AvailHeight;
+        $iframe.style.height = '100vh';
         document.body.classList.add('no-scroll');
-    } else {
-        value.height = parseInt(value.height, 10);
-        document.body.classList.remove('no-scroll');
+        return;
     }
 
-    if (isNaN(value.height)) {
+    value.height = parseInt(value.height, 10);
+    document.body.classList.remove('no-scroll');
+
+    if (Number.isNaN(value.height)) {
         req.event(1, 'Field heigth is not typeof number');
         return;
     }
 
     value.growOnly = value.growOnly !== false; // true als default
 
-    if ($iframe && (!value.growOnly || parseInt($iframe.style.height) < value.height)) {
+    if ($iframe && (!value.growOnly || parseInt($iframe.style.height, 10) < value.height)) {
         $iframe.style.height = `${value.height}px`;
     }
 }
