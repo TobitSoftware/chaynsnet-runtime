8e0a11efb0c4efa9572cc1c963c2ddf032ae3ad3
Kai Werschmöller <kai.werschmoeller@tobit.com>
2020-10-06 09:44:13 +0200

    :wrench: Use chayns-toolkit

--- src/constants/environments.js
+++ src/constants/environments.js
@@ -6,6 +6,6 @@ export const ENVIRONMENTS = {
     LIVE_V1: 'production:v1'
 };
 
-export const isLIVE = process.env.NODE_ENV === ENVIRONMENTS.LIVE;
-export const isSTAGING = process.env.NODE_ENV === ENVIRONMENTS.STAGING;
-export const isDEV = process.env.DEV === ENVIRONMENTS.DEV;
+export const isLIVE = process.env.BUILD_ENV === ENVIRONMENTS.LIVE;
+export const isSTAGING = process.env.BUILD_ENV === ENVIRONMENTS.STAGING;
+export const isDEV = process.env.BUILD_ENV === ENVIRONMENTS.DEV;
